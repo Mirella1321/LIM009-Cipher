0da5923328d82500591aa2d56f975baf000feec6
Mirella1321 <47748876+Mirella1321@users.noreply.github.com>
2019-02-27 08:51:17 -0500

    implementacion html,css,js

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,22 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+  encode: (palabra,offsetNumber) => {
+    let codificar = "";
+for (let i=0; i<palabra.length; i++) {
+let ascii = palabra.charCodeAt(i);
+  if(ascii>=65 && ascii<=90){
+codificar+=String.fromCharCode((ascii-65+offsetNumber)%26+65);
+}
+}
+return codificar;
+},
+  decode: (palabra,offsetNumber2) => {
+    let decodificar = "";
+for (let j=0; j<palabra.length; j++) {
+let valorascii = palabra.charCodeAt(j);
+  if(valorascii>=65 && valorascii<=90){
+    decodificar+=String.fromCharCode((valorascii-offsetNumber2+65)%26+65);
+    }
   }
-};
+  return decodificar;
+  }
+ };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-/* Acá va tu código */
+let cifrar = document.getElementById("cifrar");
+let desplazamiento = document.getElementById("desplazamiento").value;
+let descifrar = document.getElementById("descifrar");
+let palabra = document.getElementById("palabra").value;
+let resultado = document.getElementById("resultado");
+let limpiar = document.getElementById("limpiar");
+
+
+cifrar.addEventListener("click", () => {
+  let desplazamiento = document.getElementById("desplazamiento").value;
+  let offsetNumber = parseInt(desplazamiento,10);
+  let palabra = document.getElementById("palabra").value;
+document.getElementById("resultado").value=cipher.encode(palabra,offsetNumber);
+})
+                        
+ descifrar.addEventListener("click", () => {
+  let desplazamiento = document.getElementById("desplazamiento").value;
+  let offsetNumber2 = parseInt(desplazamiento,10);
+  let palabra = document.getElementById("palabra").value;
+document.getElementById("resultado").value=cipher.decode(palabra,offsetNumber2);
+})
