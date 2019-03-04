b05598caa2195dac7c3b1d82bcd866d84effa9fd
Mirella1321 <47748876+Mirella1321@users.noreply.github.com>
2019-03-04 12:39:07 -0500

    finalizado

--- src/cipher.js
+++ src/cipher.js
@@ -3,9 +3,9 @@ window.cipher = {
     let codificar = "";
 for (let i=0; i<palabra.length; i++) {
 let ascii = palabra.charCodeAt(i);
-  if(ascii>=65 && ascii<=90){
+  // if(ascii>=65 && ascii<=90){
 codificar+=String.fromCharCode((ascii-65+offsetNumber)%26+65);
-}
+// }
 }
 return codificar;
 },
@@ -13,10 +13,11 @@ return codificar;
     let decodificar = "";
 for (let j=0; j<palabra.length; j++) {
 let valorascii = palabra.charCodeAt(j);
-  if(valorascii>=65 && valorascii<=90){
+  // if(valorascii>=65 && valorascii<=90){
     decodificar+=String.fromCharCode((valorascii-offsetNumber2+65)%26+65);
-    }
+    // }
   }
   return decodificar;
   }
  };
+ 
